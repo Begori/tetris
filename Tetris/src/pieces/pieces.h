4d624711b168037a8c60d8035e7dc063c4f9a647
Begori <bulgakov_egor@outlook.com>
2019-02-26 10:36:39 +0300

    Implement pieces

--- Tetris/src/pieces/pieces.h
+++ Tetris/src/pieces/pieces.h
@@ -5,7 +5,10 @@ namespace pieces {
 
 	class Pieces
 	{
-
+	public:
+		int GetBlockType(int x, int y, int piece, int rotation);
+		int GetXInitialPosition(int piece, int rotation);
+		int GetYInitialPosition(int piece, int rotation);
 	};
 
 }}
