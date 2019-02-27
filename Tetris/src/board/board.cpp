ea2503045ed0dba4e1e1f5b25a827f5e3cc63108
Begori <bulgakov_egor@outlook.com>
2019-02-27 09:36:42 +0300

    init header board

--- Tetris/src/board/board.cpp
+++ Tetris/src/board/board.cpp
@@ -3,4 +3,19 @@
 namespace core {
 namespace board {
 
+	Board::Board(Pieces* pieces, int screenHeight)
+	{
+		this->pieces = pieces;
+		this->screenHeight = screenHeight;
+
+		InitBoard();
+	}
+
+	void Board::InitBoard()
+	{
+		for (int i = 0; i < BOARD_WIDRH; i++)
+			for (int j = 0; j < BOARD_HEIGHT; j++)
+				board[i][j] = POS_FREE;
+	}
+
 }}
--- Tetris/src/board/board.h
+++ Tetris/src/board/board.h
@@ -1,11 +1,40 @@
 #pragma once
+#include "..\pieces\pieces.h"
+
+#define PIECE_BLOCKS 5
+#define BOARD_POSITION 320
+#define BOARD_WIDRH 10
+#define BOARD_HEIGHT 20
+#define BOARD_LINE_WIDTH 6
+#define BLOCK_SIZE 16
+
+using namespace core::pieces;
 
 namespace core {
 namespace board {
 
 	class Board
 	{
+	public:
+		Board(Pieces* pieces, int screenHeight);
 
+		int GetXPosInPixels(int pos);
+		int GetYPosInPixels(int pos);
+
+		void StorePiece(int x, int y, int piece, int rotation);
+		void DeletePossibleLines();
+
+		bool IsFreeBlock(int x, int y);
+		bool IsPossibleMovement(int x, int y, int piece, int rotation);
+		bool IsGameOver();
+	private:
+		enum {POS_FREE, POS_FILLED};
+		int board[BOARD_WIDRH][BOARD_HEIGHT];
+		Pieces* pieces;
+		int screenHeight;
+
+		void InitBoard();
+		void DeleteLine(int y);
 	};
 
 }}
