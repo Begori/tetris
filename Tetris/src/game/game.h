787d6a2167ae17fcea7af889c7185ae9feb71ed9
Begori <bulgakov_egor@outlook.com>
2019-02-28 14:22:22 +0300

    Init header game

--- Tetris/src/game/game.h
+++ Tetris/src/game/game.h
@@ -1,11 +1,44 @@
 #pragma once
 
+#include "../board/board.h"
+#include "../pieces/pieces.h"
+#include "../io/io.h"
+#include <time.h>
+
+#define WAIT_TIME 700
+
+using namespace core::pieces;
+using namespace core::board;
+using namespace core::io;
+
 namespace core {
 namespace game {
 	
 	class Game
 	{
+	public:
+		Game(Board* board, Pieces* pieces, IO* io, int screenHeight);
 
+		int x, y;
+		int piece, rotation;
+
+		void DrawScene();
+		void CreateNewPiece();
+
+	private:
+		int screenHeight;
+		int nextX, nextY;
+		int nextPiece, nextRotation;
+
+
+		Board* board;
+		IO* io;
+		Pieces* pieces;
+
+		int GetRnd(int a, int b);
+		void InitGame();
+		void DrawBoard();
+		void DrawPiece(int x, int y, int piece, int rotation);
 	};
 
 }}
