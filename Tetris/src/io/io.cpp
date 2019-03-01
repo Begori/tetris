e5f9c9d4eb6a1266d72a6271e9146c7d47fa1bbd
Begori <bulgakov_egor@outlook.com>
2019-03-01 12:35:20 +0300

    Init header IO

--- Tetris/src/io/io.cpp
+++ Tetris/src/io/io.cpp
@@ -3,4 +3,63 @@
 namespace core {
 namespace io {
 
+	static SDL_Surface* screen;
+	static Uint32 colors[COLOR_MAX] =
+	{
+		0x000000ff,
+		0xff0000ff,
+		0x00ff00ff,
+		0x0000ffff,
+		0x00ffffff,
+		0xff00ffff,
+		0xffff00ff,
+		0xffffffff
+	};
+
+	IO::IO()
+	{
+		InitGraph();
+	}
+
+	int IO::InitGraph()
+	{
+		return 0;
+	}
+
+
+	void IO::DrawRectangle(int x1, int y1, int x2, int y2, enum color color)
+	{
+
+	}
+
+	void IO::ClearScreen()
+	{
+
+	}
+
+	void IO::UpdateScreen()
+	{
+
+	}
+
+	int IO::PollKey()
+	{
+		return 0;
+	}
+
+	int IO::GetKey()
+	{
+		return 0;
+	}
+
+	bool IO::IsKeyDown(int key)
+	{
+		return false;
+	}
+
+	int IO::GetScreenHeight()
+	{
+		return 0;
+	}
+
 }}
--- Tetris/src/io/io.h
+++ Tetris/src/io/io.h
@@ -1,10 +1,24 @@
 #pragma once
+#include <SDL.h>
 
 namespace core {
 namespace io {
-	
+	enum color {BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX};
+
 	class IO
 	{
+	public:
+		IO();
+
+		void DrawRectangle(int x1, int y1, int x2, int y2, enum color color);
+		void ClearScreen();
+		void UpdateScreen();
+
+		int GetScreenHeight();
+		int InitGraph();
+		int PollKey();
+		int GetKey();
+		bool IsKeyDown(int key);
 	};
 
 }}
